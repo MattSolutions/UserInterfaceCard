7316df689d1c7b481d618ac67581672f89cd6104
MatiBatt <battitimatias@gmail.com>
2023-09-24 16:30:42 -0300

    Initial Commit

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Matias Battiti on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
